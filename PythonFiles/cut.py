60bc7e64b00cbd70e64e3de843ff52d5afc2422f
ShearPlume <1830967718@qq.com>
2022-11-25 18:04:09 +0000

    again

--- PythonFiles/cut.py
+++ PythonFiles/cut.py
@@ -2,9 +2,10 @@ import time
 import pyperclip
 def delete_newline_char(message):
     """删除message字符串中的换行符和换行连字符"""
-    message = message.replace('. \r\n', '*-')      
+    message = message.replace('. \r\n', '*-')        
+    message = message.replace('.\r\n', '*-') 
     # 把message中的所有正常换行'.\r\n'替换成'*-'
-    message = message.replace('\r\n', ' ') 
+
     # 把message中的所有不想出现的'\r\n'替换成空格
     message = message.replace('\2', '')          
     # 删除单词中间的换行连字符
